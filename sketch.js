c223fb4e3f77b656ccc5106e2bff9232126e7ed2
Melody Jan <melody0831@gmail.com>
2017-02-14 10:48:50 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,531 @@
+function setup() {
+createCanvas(1024, 768);
+}
+
+function draw() {
+
+beginShape(); //2//
+fill(190, 48, 39);
+vertex(0,112);
+vertex(140,190);
+vertex(153,174);
+vertex(169,151);
+vertex(186,135);
+vertex(194,1260);
+veretx(207.3,115.4);
+vertex(219,106);
+vertex(238,94 254,86);
+vertex(161,0);
+vertex(0,0);
+endShape(CLOSE);
+
+
+beginShape(); //3//
+fill(97, 41, 34);
+vertex(0,184);
+vertex(110,284);
+vertex(114.1,265.5); 
+vertex(118,248);
+vertex(127,221); 
+vertex(135,199); 
+vertex(140,190); 
+vertex(0,112);
+endShape(CLOSE);
+
+beginShape(); //4//
+fill(184, 180, 172);
+vertex(161,0);
+vertex(254,86); 
+vertex(266,80.5);
+vertex(271.5,79); 
+vertex(279,76.5);
+vertex(287,73.5);
+vertex(296.5,70.5);
+vertex(307,67.5); 
+vertex(318,65); 
+vertex(327.5,63.5); 
+vertex(340,62.5);
+vertex(349.5,62); 
+vertex(360.5,62);
+vertex(375.5,61.5); 
+vertex(389,62.5);
+vertex(402.5,64);
+vertex(418,67);
+vertex(430,70);
+vertex(446.5,76);
+vertex(463,82);
+vertex(477.5,89); 
+vertex(492.5,95);
+vertex(504,102);
+vertex(516.5,109.5);
+vertex(527.5,118); 
+vertex(534,125); 
+vertex(545.5,135);
+vertex(557,147.5);
+vertex(570.5,162.5);
+vertex(578.5,175.5);
+vertex(583.5,183);
+vertex(591.5,198);
+vertex(598,210.5);
+vertex(603,221);
+vertex(609,235.5);
+vertex(713.5,0);
+endShape(CLOSE);
+
+beginShape(); //5//
+fill(155, 154, 148);
+vertex(609,235.5);
+vertex(557,357.5);
+vertex(566,367.3); 
+vertex(621.5,294.5);
+vertex(621.5,287);
+vertex(620.2,279.2);
+vertex(618.8,270.3);
+vertex(616.5,260);
+vertex(614.8,253.8);
+vertex(612.8,245.5);
+vertex(610.5,239.3);
+endShape(CLOSE);
+       
+beginShape(); //6//
+fill(183, 135, 115);
+vertex(566,367.3);
+vertex(534.8,408.3);
+vertex(557,357.5);
+endShape(CLOSE);
+
+beginShape(); //7//
+fill(194, 138, 119);
+vertex(621.5,294.5); 
+vertex(622.8,300.9); 
+vertex(622.8,303.6); 
+vertex(622.8,305.9); 
+vertex(622.9,310.7); 
+vertex(622.9,316.3); 
+vertex(622.9,319.3); 
+vertex(623.3,328.7); 
+vertex(623,332.6); 
+vertex(622.4,346.1); 
+vertex(621.5,353.3); 
+vertex(620.3,359.6); 
+vertex(619.3,365.7); 
+vertex(618.1,372); 
+vertex(617.9,375.4); 
+vertex(616.6,380); 
+vertex(614.8,387.3); 
+vertex(612.9,394.5); 
+vertex(611.6,399.4); 
+vertex(609.3,404.6); 
+vertex(566,367.3);
+endShape(CLOSE);
+
+beginShape(); //8//
+fill(25, 24, 28);
+vertex(609,235.9);
+vertex(609.9,237.8); 
+vertex(611.4,241.9); 
+vertex(613.2,247.5); 
+vertex(614.9,254.2); 
+vertex(616.3,259.3); 
+vertex(618.6,269.4); 
+vertex(619.8,276.7); 
+vertex(620.8,282.6); 
+vertex(621.5,287.3); 
+vertex(621.5,292.6); 
+vertex(621.5,295); 
+vertex(842.7,0); 
+vertex(712.8,0);
+endShape(CLOSE);
+
+beginShape(); //9//
+fill(17, 20, 28)
+vertex(110,284.8); 
+vertex(107.8,297.3);
+vertex(107.5,323); 
+vertex(108.3,331); 
+vertex(109,346.3); 
+vertex(110,358.5); 
+vertex(113,367.8); 
+vertex(116.8,381.5); 
+vertex(123.5,400.3); 
+vertex(129,415.3); 
+vertex(138.5,433.3); 
+vertex(142.5,445); 
+vertex(151.8,457.5); 
+vertex(160,468.3);  
+vertex(167.5,478.5); 
+vertex(178.8,489.8); 
+vertex(190.5,502.3);  
+vertex(201.3,512.3);  
+vertex(216,523.8);  
+vertex(220.5,527.5);  
+vertex(0,405.8);  
+vertex(0,184.5); 
+endShape(CLOSE);
+
+beginShape(); //10//
+fill(167, 169, 160);
+vertex(0,405.8);
+vertex(381,615.5); 
+vertex(266,768); 
+vertex(0,768);
+endShape(CLOSE);
+    
+beginShape(); //11//
+fill(187, 53, 38);
+vertex(381,615.5);
+vertex(433,643.5);
+vertex(381,768);
+vertex(381,768);
+vertex(266,768);
+endShape(CLOSE);
+
+beginShape(); //12//
+fill(90, 37, 31);
+vertex(433,643.3);
+vertex(586.5, 728.3);
+vertex(556.8, 768);
+vertex(380.8, 767.8);
+endShape(CLOSE);
+
+beginShape(); //13//
+fill(18, 17, 20);
+vertex(586.5,728.5); 
+vertex(631.5,768);
+vertex(557,768);
+endShape(CLOSE);
+       
+beginShape(); //14//
+fill(91, 31, 24);
+vertex(586.5,728.5);
+vertex(637.5,755.5);
+vertex(631.5,768);
+endShape(CLOSE);
+    
+beginShape(); //15//
+fill(173, 53, 36);
+vertex(637.5,755.5);
+vertex(662,768);
+vertex(631.5,768);
+endShape(CLOSE);
+
+beginShape(); //16//
+fill(77, 36, 29)
+vertex(416.5,569.5);
+vertex(451,601); 
+vertex(433,643.5); 
+vertex(381,615.5);
+endShape(CLOSE);
+
+beginShape(); //16-1//
+fill(100, 39, 34);
+vertex(451,601); 
+vertex(586.5,728.5); 
+vertex(433,643.5);
+endShape(CLOSE);
+    
+beginShape(); //17//
+fill(190, 48, 38);
+vertex(967,0);
+vertex(1024,0);
+vertex(1024,136);
+vertex(740,521);
+endShape(CLOSE);
+
+beginShape(); //18//
+fill(168, 164, 153);
+vertex(738.3,523.7);
+vertex(1024,681.7); 
+vertex(1024,136);
+endShape(CLOSE);
+
+beginShape(); //19//
+fill(24, 22, 27);
+vertex(1024,681.7);
+vertex(1024,768); 
+vertex(1006,768); 
+vertex(738.3,523.7);
+endShape(CLOSE);
+
+beginShape(); //20//
+fill(92, 37, 30);
+vertex(738.3,523.7); 
+vertex(637.5,755.5); 
+vertex(662,768); 
+vertex(1006,768);
+endShape(CLOSE);
+
+beginShape(); //21//
+fill(78, 38, 30);
+vertex(738.3,523.7); 
+vertex(586.5,728.5); 
+vertex(637.5,755.5);
+endShape(CLOSE);
+
+beginShape(); //22//
+fill(135, 52, 31);
+vertex(738.3,523.7);
+vertex(609.5,404);
+vertex(607.3,410.7); 
+vertex(604,417.3); 
+vertex(600.7,425); 
+vertex(597.7,431.7); 
+vertex(593.3,439);
+endShape(CLOSE);
+
+beginShape(); //23//
+fill(207, 134, 118);
+vertex(566.3,367.3);
+vertex(533.1,411.5);
+vertex(542.8,415.4); 
+vertex(593.3,439.2); 
+vertex(594.5,437.1); 
+vertex(596.4,433.7); 
+vertex(598.2,430.5); 
+vertex(599.5,427.5);
+vertex(600.8,424.7);
+vertex(602.4,421.1);
+vertex(604.1,417.1);
+vertex(606.1,413.2);
+vertex(607.8,408.3);
+vertex(609.3,404.6);
+endShape(CLOSE);
+
+beginShape(); //24//
+fill(204, 134, 117);	
+vertex(533.1,412.8); 
+vertex(474,550);
+vertex(470.3,551.6);
+vertex(467.2,553.1);
+vertex(461.9,554.8);
+vertex(458.3,556.4);
+vertex(453.3,558.1);
+vertex(447.9,560.1); 
+vertex(444.1,561.8); 
+vertex(438.7,562.9); 
+vertex(433.4,564.3);
+vertex(427,566.6);
+vertex(421.9,567.8);
+vertex(416.7,569.1);
+vertex(533.1,411.5); 
+vertex(533.7,411.7);
+endShape(CLOSE);
+    
+beginShape(); //25//
+fill(141, 42, 28);
+vertex(451,601); 
+vertex(416.5,569.2); 
+vertex(421.7,567.9); 
+vertex(427.1,566.5); 
+vertex(432.6,564.6); 
+vertex(438.4,563); 
+vertex(447.5,560.5); 
+vertex(458.1,556.5); 
+vertex(461.3,555.1); 
+vertex(465.1,553.9); 
+vertex(468.5,552.5); 
+vertex(470.3,551.5); 
+vertex(474,550);
+endShape(CLOSE);
+    
+beginShape(); //26/
+fill(19, 18, 22);
+vertex(416.5,569.5); 
+vertex(409.5,570.6); 
+vertex(396.9,572.3); 
+vertex(378.4,572.3); 
+vertex(364.4,572.3); 
+vertex(346.8,572.3); 
+vertex(334.5,571.6); 
+vertex(325.8,570.9); 
+vertex(318.3,569.1); 
+vertex(306.6,565.6); 
+vertex(294.8,561.8); 
+vertex(285.1,558.9);
+vertex(277.8,556.1); 
+vertex(271.3,553.3); 
+vertex(264.3,551.3); 
+vertex(381,615.5);
+endShape(CLOSE);
+    
+beginShape(); //27//
+fill(119, 43, 30);
+vertex(474,550); 
+vertex(451,601); 
+vertex(586.5,728.5); 
+vertex(738.3,523.7); 
+vertex(593.3,439.2); 
+vertex(590.8,444.5); 
+vertex(586.5,454); 
+vertex(578,463.8); 
+vertex(571.2,475); 
+vertex(561.8,484); 
+vertex(554,493.5); 
+vertex(536.5,510); 
+vertex(528.3,517.3); 
+vertex(517.3,525); 
+vertex(507.8,531.8); 
+vertex(495,539.3); 
+vertex(485.8,544.5);
+endShape(CLOSE);
+    
+beginShape(); //28//
+fill(92, 37, 30);
+vertex(621.5,295); 
+vertex(622.5,299.8); 
+vertex(622.8,305); 
+vertex(622.9,311.3); 
+vertex(622.9,320.5); 
+vertex(623.3,332.1); 
+vertex(622.4,345.5); 
+vertex(621.2,355.1); 
+vertex(618.4,370.6); 
+vertex(616.2,381.7); 
+vertex(612.1,397.6); 
+vertex(609.3,404.6); 
+vertex(738.3,523.7); 
+vertex(967.6,0); 
+vertex(842.1,0);
+endShape(CLOSE);
+    
+beginShape(); //29//
+fill(193, 101, 82);
+vertex(593,439.8);
+vertex(591.4,443.1); 
+vertex(588.8,448.9); 
+vertex(587.3,452.3); 
+vertex(584.9,455.8); 
+vertex(578.8,463.8); 
+vertex(568.8,477.3); 
+vertex(555.1,492.2); 
+vertex(530.5,515.3); 
+vertex(510.1,530.1); 
+vertex(500.4,536.1); 
+vertex(484.6,545); 
+vertex(477.2,548.5); 
+vertex(474,550); 
+vertex(533.7,411.7);
+endShape(CLOSE);
+    
+beginShape(); //30//
+fill(182, 138, 98);
+vertex(110,284); 
+vertex(416.7,569.1); 
+vertex(533.7,411.7); 
+vertex(140,190); 
+vertex(137.7,194.1); 
+vertex(134.9,199.4); 
+vertex(132.8,205.1); 
+vertex(130.4,211.6); 
+vertex(128.5,216.9); 
+vertex(127,221); 
+vertex(125.9,224.2); 
+vertex(124.7,227.9); 
+vertex(123.6,231.2); 
+vertex(122.5,234.4); 
+vertex(121.6,237.2); 
+vertex(120.7,239.9); 
+vertex(119.4,243.7); 
+vertex(118.3,247.1); 
+vertex(117.8,248.9); 
+vertex(117.3,251.1); 
+vertex(116.3,255); 
+vertex(115.7,257.5); 
+vertex(115.1,259.8); 
+vertex(114,265.2); 
+vertex(113.1,269.1); 
+vertex(112.1,273.6); 
+vertex(111.5,276.1);
+vertex(110.6,280.4);
+endShape(CLOSE);
+    
+beginShape(); //31//
+fill(234, 177, 157);
+vertex(140,190); 
+vertex(143.3,185.9); 
+vertex(149.2,178.6); 
+vertex(158.2,166.5); 
+vertex(164.1,158.1); 
+vertex(171.1,149.1); 
+vertex(175.7,144.7); 
+vertex(182.3,138.5); 
+vertex(192.8,127.4); 
+vertex(201.2,120.2); 
+vertex(212.8,111); 
+vertex(223.2,103.3); 
+vertex(233.6,96.8); 
+vertex(240.9,92.6); 
+vertex(250.1,88); 
+vertex(254,86); 
+vertex(557,357.5); 
+vertex(533.1,411.5);
+endShape(CLOSE);
+    
+beginShape(); //32//
+fill(141, 142, 128);
+vertex(110,284); 
+vertex(107.8,297.3); 
+vertex(107.6,317); 
+vertex(107.6,341); 
+vertex(110,358.5); 
+vertex(116.8,381.5); 
+vertex(122.5,397.3); 
+vertex(129,415.3); 
+vertex(138,432.3); 
+vertex(145,448.4); 
+vertex(156.6,463.8); 
+vertex(167.5,478.5);
+vertex(179.8,490.8);
+vertex(197,508.3);
+vertex(212.5,521);
+vertex(220.5,527.5); 
+vertex(266,552.2); 
+vertex(285.1,558.9); 
+vertex(311.2,567); 
+vertex(336.5,571.7);
+vertex(362,572.3); 
+vertex(392.5,572.3); 
+vertex(416.5,569.5);
+endShape(CLOSE);
+    
+beginShape(); //33//
+fill(227, 220, 205);
+vertex(557,357.5); 
+vertex(609,235.5);
+vertex(605.8,227.8); 
+vertex(601,216.8);
+vertex(593,201); 
+vertex(580.4,178.4); 
+vertex(570.5,162.5); 
+vertex(563.5,154.8); 
+vertex(554.8,145.1); 
+vertex(546.4,136);
+vertex(540.5,130.6); 
+vertex(534.8,124); 
+vertex(527.5,118); 
+vertex(516.5,109.5); 
+vertex(502.9,101.3); 
+vertex(492.5,95); 
+vertex(482.6,91); 
+vertex(474.8,87.7); 
+vertex(468.1,84.5); 
+vertex(453.8,78.7); 
+vertex(444.2,75.2); 
+vertex(431.5,70.5); 
+vertex(413.3,66.1); 
+vertex(389,62.5); 
+vertex(371,61.6); 
+vertex(365.3,61.6); 
+vertex(353,62); 
+vertex(346.3,62.2); 
+vertex(332.4,63.1); 
+vertex(324.2,64); 
+vertex(306.4,67.7); 
+vertex(291.8,72); 
+vertex(281.1,75.7); 
+vertex(271.5,79); 
+vertex(264.6,81.2); 
+vertex(254,86);
+endShape(CLOSE);
+    
+}
